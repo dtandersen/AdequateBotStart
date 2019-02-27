0da054504e74aaa6fd5a4d1b6f368840e1466606
Christopher Gorman <christophergorman@yahoo.com>
2019-02-26 19:43:39 -0700

    0.17 Update

--- control.lua
+++ control.lua
@@ -25,16 +25,12 @@ script.on_event(defines.events.on_player_created, function(event)
 
 	local player = game.players[event.player_index]
 	for i, v in pairs(items) do
-        if data.raw["item"][v[1]] then
-            player.insert{name = v[1], count = v[2]}
-        end
+		player.insert{name = v[1], count = v[2]}
     end
 
 	local grid = player.get_inventory(defines.inventory.player_armor)[1].grid
 	for  i, v in pairs(armor) do
-        if data.raw["armor"][v[1]] then
-		    grid.put({name = v[1]})
-        end if
+		grid.put({name = v[1]})
 	end
 
 end)
