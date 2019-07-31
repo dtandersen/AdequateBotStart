9adb7f122ae6b40b5d79c5ac5925a8bc57b896f4
Tanel Tumanski <tanel.tumanski@gmail.com>
2019-07-31 18:56:10 +0300

    Update script to reflect API changes (#1)
    player_armor -> character_armor

--- control.lua
+++ control.lua
@@ -28,7 +28,7 @@ script.on_event(defines.events.on_player_created, function(event)
 		player.insert{name = v[1], count = v[2]}
     end
 
-	local grid = player.get_inventory(defines.inventory.player_armor)[1].grid
+	local grid = player.get_inventory(defines.inventory.character_armor)[1].grid
 	for  i, v in pairs(armor) do
 		grid.put({name = v[1]})
 	end
